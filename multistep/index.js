0b813c290a831515c96d08868ccc645fb7bd483c
uwx <maxine@cybergal.com>
2023-10-08 01:57:06 -0300

    Improve2

--- multistep/index.js
+++ multistep/index.js
@@ -4,4 +4,4 @@ import { setFailed } from '@actions/core';
 import { exec } from '@actions/exec';
 import { resolve } from 'path';
 
-exec('pwsh', ['-command', `. '${resolve(__dirname, 'index.ps1')}'`]).catch(err => setFailed(err.message));
+exec('pwsh', ['-command', `& { & '${resolve(__dirname, 'index.ps1')}' } | Out-Default`]).catch(err => setFailed(err.message));
